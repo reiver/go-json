54ccfccab8efbe5f6ad2009f188446f712d2f59c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-08-08 08:45:07 -0700

    json.DefaultUsher

---
+++ defaultusher.go
@@ -0,0 +1,12 @@
+package json
+
+var (
+	// DefaultUsher is the Usher that the json.Marshal() function uses.
+	//
+	// Implanting a modifier into DefaultUsher into DefaultUsher modifies how the json.Marshal() behaves.
+	DefaultUsher Usher
+)
+
+func init() {
+	DefaultUsher.ImplantModifier("string", StringModifierFunc)
+}
