04183c85e77a50b1d718e3d1661d1336946494bc
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-06 13:02:24 -0800

    reorganizing code

--- usher.go
+++ usher.go
@@ -47,45 +47,6 @@ func (receiver *Usher) ImplantModifier(name string, fn ModifierFunc) {
 	})
 }
 
-// Marshal returns the JSON version of 'value'.
-func (receiver *Usher) Marshal(value any) ([]byte, error) {
-	if nil == value {
-		return []byte{'n','u','l','l'}, nil
-	}
-
-	switch casted := value.(type) {
-	case Marshaler:
-		return casted.MarshalJSON()
-	case encoding.TextMarshaler:
-		bytes, err := casted.MarshalText()
-		if nil != err {
-			return nil, err
-		}
-
-		var str string = string(bytes)
-		return gojson.Marshal(str)
-	case string:
-		return MarshalString(casted), nil
-	default:
-		reflectedType := reflect.TypeOf(value)
-		if nil == reflectedType {
-			return nil, errBadReflection
-		}
-
-		switch reflectedType.Kind() {
-//@TODO: array
-		case reflect.Struct:
-			return receiver.marshalStruct(value)
-		case reflect.Slice:
-			return receiver.marshalSlice(value)
-		case reflect.Map:
-			return receiver.marshalMap(value)
-		default:
-			return gojson.Marshal(value)
-		}
-	}
-}
-
 func (receiver *Usher) marshalMap(value any) ([]byte, error) {
 	if nil == value {
 		return []byte{'n','u','l','l'}, nil
