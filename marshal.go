f1ae917e6f87758eccc4deac2024b00338b728a3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-11 03:50:14 -0800

    docs

--- marshal.go
+++ marshal.go
@@ -1,6 +1,8 @@
 package json
 
 // Marshal return the JSON version of 'value'.
+//
+// See [Usher.Marshal] for more information.
 func Marshal(value any) ([]byte, error) {
 	return DefaultUsher.Marshal(value)
 }
