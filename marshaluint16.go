286e3739b188da6558a418a8bd5856f6522696c1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-10 16:55:43 -0800

    json.MarshalUint16()

--- marshaluint16.go
+++ marshaluint16.go
@@ -1,6 +1,6 @@
 package json
 
-// MarshalUint64 returns the JSON version of a Go uint64.
+// MarshalUint16 returns the JSON version of a Go uint64.
 func MarshalUint16(value uint16) []byte {
 	return MarshalUint64(uint64(value))
 }
