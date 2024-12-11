983381966b211424d74464f78ecb7eb88e86a47e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-11 13:41:16 -0800

    docs

--- emptier.go
+++ emptier.go
@@ -1,5 +1,32 @@
 package json
 
+// Emptier works with the `omitempty` struct-tag, recognized by [Marshal] and [Usher.Marshal].
+//
+// For example, a custom type might look like:
+//
+//	type MyType struct {
+//		// ...
+//	}
+//	
+//	func (receiver MyType) IsEmpty() bool {
+//		// ...
+//	}
+//
+// And it might be used similar to the following:
+//
+//	var MyStruct struct {
+//		Apple  string `json:"apple,omitempty"`
+//		Banana MyType `json:"banana,omitempty"` // <---------
+//		Cherry int    `json:"cherry"`
+//	}
+//	
+//	// ...
+//
+//	var value MyStruct // = ...
+//	
+//	bytes, err := json.Marshal(value)
+//
+// An alternative to Emptier is [Nothinger], which is more commonly usef with optional-types (i.e., optiona-types).
 type Emptier interface {
 	IsEmpty() bool
 }
