838f3e4d27dfaab019789eec5d1dd79aeaeb63a4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-13 02:52:08 -0800

    docs

--- marshal.go
+++ marshal.go
@@ -2,7 +2,21 @@ package json
 
 // Marshal returns the JSON version of 'value'.
 //
+// For example:
+//
+//	bytes, err := json.Marshal(value)
+//
 // See [Usher.Marshal] for more information.
+//
+// Calling:
+//
+//	bytes, err := json.Marshal(value)
+//
+// Is the same as calling:
+//
+//	bytes, err := json.DefaultUsher.Marshal(value)
+//
+// json.Marshal() is a convenience function for json.DefaultUsher.Marshal().
 func Marshal(value any) ([]byte, error) {
 	return DefaultUsher.Marshal(value)
 }
