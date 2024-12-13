d0a5c34d5a1c12f1d63a770cd7bd1a51cb490d0e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-13 02:04:47 -0800

    docs

--- nothinger.go
+++ nothinger.go
@@ -1,5 +1,14 @@
 package json
 
+// Nothinger is similar to [Emptier], but is more commonly used with optional-types (i.e., optiona-types).
+//
+// I.e., an optional-type has "something" in it or "nothing" in it.
+// (Some optional-types call this "some" and "none".)
+// A optional-type might have a method IsNothing()bool to communicate whether it has "nothing" or "something" in it.
+//
+// If the optional-type has such a IsNothing()bool method, then [Marshal] and [Usher.Marshal] make use of that method for the purposes of `omitempty`.
+//
+// If you type in not an optional-type, then it should probably instead implement [Emptier].
 type Nothinger interface {
 	IsNothing() bool
 }
