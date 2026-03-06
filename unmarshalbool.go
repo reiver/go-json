7e1683a3800c6f983d007755f1c9517128007653
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-06 07:14:39 -0800

    unmarshal bool

--- unmarshalbool.go
+++ unmarshalbool.go
@@ -16,6 +16,7 @@ func UnmarshalBool(bytes []byte, dst *bool) error {
 	bytes = trim(bytes)
 
 	var value bool
+
 	switch {
 	case gobytes.Equal(bytes, falseBytes):
 		value = false
@@ -31,12 +32,43 @@ func UnmarshalBool(bytes []byte, dst *bool) error {
 }
 
 // UnobstructedUnmarshal JSON-unmarshals a JSON bool into a Go bool.
-// It ignore the letter-casing of "true" and "false".
+// It ignore the letter-casing of "true" and "false",
+// (also) accepts "t" and "f",
+// and (also) accepts "1" and "0".
 func UnobstructedUnmarshalBool(bytes []byte, dst *bool) error {
-	var lower []byte = gobytes.ToLower(bytes)
-	err := UnmarshalBool(lower, dst)
-	if nil != err {
+	if len(bytes) <= 0 {
+		var err error = ErrNotBool
 		return erorr.Errorf("json: cannot parse %q as a 'unobstructed' JSON bool: %w", bytes, err)
+	}
+
+	var lower []byte = gobytes.ToLower(trim(bytes))
+	{
+		var byte0 byte = bytes[0]
+
+		switch byte0 {
+		case '0', '1', '.':
+			lower = NormalizeNumberBytes(lower)
+		}
+	}
+
+	if 1 == len(lower) {
+		switch lower[0] {
+		case 'f', '0':
+			value := false
+			*dst = value
+			return nil
+		case 't', '1':
+			value := true
+			*dst = value
+			return nil
+		}
+	}
+
+	{
+		err := UnmarshalBool(lower, dst)
+		if nil != err {
+			return erorr.Errorf("json: cannot parse %q as a 'unobstructed' JSON bool: %w", bytes, err)
+		}
 	}
 
 	return nil
--- unmarshalbool_test.go
+++ unmarshalbool_test.go
@@ -43,6 +43,237 @@ func TestUnmarshalBool(t *testing.T) {
 	}
 }
 
+func TestUnobstructedUnmarshalBool(t *testing.T) {
+	tests := []struct{
+		JSON     []byte
+		Expected bool
+	}{
+		{
+			JSON: []byte(`true`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`false`),
+			Expected:     false,
+		},
+
+
+
+		{
+			JSON: []byte(`TRUE`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`FALSE`),
+			Expected:     false,
+		},
+
+
+
+		{
+			JSON: []byte(`True`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`False`),
+			Expected:     false,
+		},
+
+
+
+		{
+			JSON: []byte(`tRuE`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`fAlSe`),
+			Expected:     false,
+		},
+
+
+
+		{
+			JSON: []byte(`t`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`f`),
+			Expected:     false,
+		},
+		{
+			JSON: []byte(`T`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`F`),
+			Expected:     false,
+		},
+
+
+
+		{
+			JSON: []byte(`1`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`0`),
+			Expected:     false,
+		},
+
+		{
+			JSON: []byte(`1.`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`0.`),
+			Expected:     false,
+		},
+		{
+			JSON: []byte(`1.0`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`0.0`),
+			Expected:     false,
+		},
+		{
+			JSON: []byte(`1.00`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`0.00`),
+			Expected:     false,
+		},
+
+		{
+			JSON: []byte(`01`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`00`),
+			Expected:     false,
+		},
+		{
+			JSON: []byte(`01`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`00`),
+			Expected:     false,
+		},
+
+		{
+			JSON: []byte(`00001.`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`00000.`),
+			Expected:     false,
+		},
+		{
+			JSON: []byte(`00001.0000`),
+			Expected:     true,
+		},
+		{
+			JSON: []byte(`00000.0000`),
+			Expected:     false,
+		},
+	}
+
+	for testNumber, test := range tests {
+		var actual bool
+		err := json.UnobstructedUnmarshalBool(test.JSON, &actual)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error but actually got one.", testNumber)
+			t.Logf("ERROR: %s", err)
+			t.Logf("JSON: (%d)\n%s", len(test.JSON), test.JSON)
+			continue
+		}
+
+		expected := test.Expected
+
+		if expected != actual {
+			t.Errorf("For test #%d, the actual JSON-unmarshaled %T is not what was expected.", testNumber, actual)
+			t.Logf("EXPECTED: %t", expected)
+			t.Logf("ACTUAL:   %t", actual)
+			t.Logf("JSON: (%d)\n%s", len(test.JSON), test.JSON)
+			continue
+		}
+	}
+}
+
+func TestUnobstructedUnmarshalBool_fail(t *testing.T) {
+	tests := []struct{
+		JSON []byte
+	}{
+		{
+			JSON: nil,
+		},
+		{
+			JSON: []byte(""),
+		},
+
+
+
+		{
+			JSON: []byte(`false,`),
+		},
+		{
+			JSON: []byte(`true,`),
+		},
+		{
+			JSON: []byte(`false]`),
+		},
+		{
+			JSON: []byte(`true]`),
+		},
+		{
+			JSON: []byte(`false}`),
+		},
+		{
+			JSON: []byte(`true}`),
+		},
+
+
+
+		{
+			JSON: []byte(`tr`),
+		},
+		{
+			JSON: []byte(`fa`),
+		},
+
+
+
+		{
+			JSON: []byte(`11`),
+		},
+		{
+			JSON: []byte(`10`),
+		},
+
+
+
+		{
+			JSON: []byte(`"true"`),
+		},
+		{
+			JSON: []byte(`"false"`),
+		},
+	}
+
+	for testNumber, test := range tests {
+		var value bool
+		err := json.UnobstructedUnmarshalBool(test.JSON, &value)
+		if nil == err {
+			t.Errorf("For test #%d, expected an error but did not actually get one.", testNumber)
+			t.Logf("JSON: (%d)\n%s", len(test.JSON), test.JSON)
+			continue
+		}
+
+	}
+}
+
 func TestUnmarshalBool_fail(t *testing.T) {
 	tests := []struct{
 		JSON []byte
