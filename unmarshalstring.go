a86d3e06d1f6031b1b1124f9a40fa0e8cc79aa86
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-06 07:24:00 -0800

    unmarshal string

--- unmarshalstring.go
+++ unmarshalstring.go
@@ -1,10 +1,12 @@
 package json
 
-import (
-	gojson "encoding/json"
-)
-
 // UnmarshalString JSON-unmarshals a JSON string into a Go string.
 func UnmarshalString(data []byte, dst *string) error {
-	return gojson.Unmarshal(data, dst)
+	str, err := unquoteString(data)
+	if nil != err {
+		return err
+	}
+
+	*dst = str
+	return nil
 }
